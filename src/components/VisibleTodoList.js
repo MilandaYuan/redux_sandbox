22d84e24e60cec7af4f67a025fe1c7cf9dff3b81
MilandaYuan <nwhals@163.com>
2018-03-06 16:42:49 +0800

    lesson14-part2-Fetching Data on Route Change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,19 +1,40 @@
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import TodoList from "./TodoList";
 import { toggleTodo } from "../actions/index";
 import { withRouter } from "react-router";
 import { getVisibleTodos } from "../reducers/index";
+import { fetchTodos } from "../api/index";
 
-const mapStateToTodoListProps = (state, { params }) => ({
-  todos: getVisibleTodos(state, params.filter || "all")
-});
-// const mapDispatchToTodoListProps = dispatch => ({
-//   onTodoClick(id) {
-//     dispatch(toggleTodo(id));
-//   }
-// });
-const VisibleTodoList = withRouter(
-  connect(mapStateToTodoListProps, { onTodoClick: toggleTodo })(TodoList)
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  }
+  componentDidUpdate(preProps) {
+    if (this.props.filter !== preProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      );
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
+
+const mapStateToTodoListProps = (state, { params }) => {
+  const filter = params.filter || "all";
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  };
+};
+
+VisibleTodoList = withRouter(
+  connect(mapStateToTodoListProps, { onTodoClick: toggleTodo })(VisibleTodoList)
 );
 
 export default VisibleTodoList;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import configureStore from "./configureStore";
 import Root from "./components/Root";
-import { fetchTodos } from "./api/index";
 
 const store = configureStore();
 render(<Root store={store} />, document.getElementById("root"));
-fetchTodos("all").then(todos => console.log(todos));
